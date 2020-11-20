7f6e09c34006974272b21ccfbe0c5897cd88686e
Yuhtin <rio.daviduarte@gmail.com>
2020-11-20 16:05:28 -0300

    added(plugin): starting plugin

---
+++ src/main/java/com/yuhtin/minecraft/animations/Animations.java
@@ -0,0 +1,17 @@
+package com.yuhtin.minecraft.animations;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Animations extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
